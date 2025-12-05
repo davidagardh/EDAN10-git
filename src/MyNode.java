4df7502a904f1e24f4bcfa8d133bcd0cd0595451
David Agardh <agardh.david@gmail.com>
2025-12-05 09:26:19 +0100

    Small fix after release

--- src/MyNode.java
+++ src/MyNode.java
@@ -3,6 +3,7 @@ package tree;
 // 2) Two changes different files no pull before
 // 2) Two changes, same file, different ends, no pull before
 // 2) Two changes, same file, different ends, pull before
+// Small fix to move past v1.0
 
 /**
  * This is a node in a binary search tree. The node holds an integer value.
